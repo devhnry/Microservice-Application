acd25722c125c2bf440f81511c1d09e80af3fc00
devhnry <devwhenry@gmail.com>
2024-06-03 11:07:00 +0100

    Change configuration to read config from a gitHub Repo and update on live changes

--- v2-cloud-config/accounts/src/main/java/org/microservicebank/accounts/AccountsApplication.java
+++ v2-cloud-config/accounts/src/main/java/org/microservicebank/accounts/AccountsApplication.java
@@ -4,12 +4,10 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
-import jdk.jfr.Enabled;
 import org.microservicebank.accounts.dto.AccountContactInfoDto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
--- v2-cloud-config/accounts/src/main/java/org/microservicebank/accounts/dto/AccountContactInfoDto.java
+++ v2-cloud-config/accounts/src/main/java/org/microservicebank/accounts/dto/AccountContactInfoDto.java
@@ -1,16 +1,17 @@
 package org.microservicebank.accounts.dto;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.Map;
 
 //@Component
 @ConfigurationProperties(prefix = "accounts")
-public record AccountContactInfoDto(
-        String message,
-        Map<String,String> contactDetails,
-        List<String> onCallSupport
-) {
+@Getter @Setter
+public class AccountContactInfoDto{
+    String message;
+    Map<String,String> contactDetails;
+    List<String> onCallSupport;
 }

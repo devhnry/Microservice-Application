cf8952482f7a6575a3cc656833ac42a5747be4bb
devhnry <devwhenry@gmail.com>
2024-06-03 14:59:52 +0100

    Reconfiguration

--- v2-cloud-config/cards/src/main/java/org/microservicebank/cards/exceptions/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package org.microservicebank.cards.exceptions;
-
-public class ResourceNotFoundException extends RuntimeException{
-    public ResourceNotFoundException(String resourceName, String fieldName, String fieldValue) {
-        super(String.format("%s not found with the input data %s, %s", resourceName, fieldName, fieldValue));
-    }
-}
